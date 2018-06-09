0679373e170c78fd2b9a3a8fd47b270426669f4a
gnorambuena <gabo.norambuena@gmail.com>
2018-06-09 12:36:37 -0400

    Added code for folder creation

--- Download.py
+++ Download.py
@@ -1,8 +1,14 @@
 import wget
+import os
+import shutil
 
 url = "http://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/"
+path = "data"
+if os.path.isdir("/"+path):
+    shutil.rmtree("/"+path)
+os.makedirs(path)
 
 with open("classes.txt", "r") as file:
     for s in file.readlines():
         s = s[:-1]
-        wget.download(url + s + ".npy", "data/" + s + ".npy")
+        wget.download(url + s + ".npy", data + "/" + s + ".npy")
