d69eb528e65f49c46deef80d13feb7f506f6eddf
JMarinkovic <javier.marinkovic95@gmail.com>
2018-06-19 20:43:39 -0400

    Agregar with tf.device('/device:GPU:0'):

--- Main.py
+++ Main.py
@@ -3,31 +3,33 @@ import numpy as np
 import os
 from Model import *
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
 
-train_n = 100000
-train_data = np.load("traindata.npy").astype(float)
-train_data = np.reshape(train_data,(train_n,img_size,img_size,channels))
-train_label = np.load("trainlabel.npy").astype(int)
+with tf.device('/device:GPU:0'):
+	config = tf.ConfigProto()
+	config.gpu_options.per_process_gpu_memory_fraction = 0.4
 
-test_n = 5000
-test_data = np.load("testdata.npy").astype(float)
-test_data = np.reshape(test_data,(test_n,img_size,img_size,channels))
-test_label = np.load("testlabel.npy").astype(int)
+	train_n = 100000
+	train_data = np.load("traindata.npy").astype(float)
+	train_data = np.reshape(train_data,(train_n,img_size,img_size,channels))
+	train_label = np.load("trainlabel.npy").astype(int)
 
-batch_size = 1
-epochs = 50
-dataset = tf.data.Dataset.from_tensor_slices((train_data,train_label))
-dataset = dataset.shuffle(buffer_size = train_n, reshuffle_each_iteration = True)
-dataset = dataset.batch(batch_size)
-dataset = dataset.repeat(epochs)
-iterator = dataset.make_one_shot_iterator()
-next_element = iterator.get_next()
+	test_n = 5000
+	test_data = np.load("testdata.npy").astype(float)
+	test_data = np.reshape(test_data,(test_n,img_size,img_size,channels))
+	test_label = np.load("testlabel.npy").astype(int)
 
-init = tf.global_variables_initializer()
+	batch_size = 1
+	epochs = 50
+	dataset = tf.data.Dataset.from_tensor_slices((train_data,train_label))
+	dataset = dataset.shuffle(buffer_size = train_n, reshuffle_each_iteration = True)
+	dataset = dataset.batch(batch_size)
+	dataset = dataset.repeat(epochs)
+	iterator = dataset.make_one_shot_iterator()
+	next_element = iterator.get_next()
 
-with tf.Session(config = config) as sess:
-    sess.run(init)
-    x,y_label = sess.run(next_element)
-    sess.run(optimizer)
+	init = tf.global_variables_initializer()
+
+	with tf.Session(config = config) as sess:
+	    sess.run(init)
+	    x,y_label = sess.run(next_element)
+	    sess.run(optimizer)
--- Model.py
+++ Model.py
@@ -1,50 +1,53 @@
 import tensorflow as tf
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
-def buildConv2DLayer(input, out_size):
-	conv = tf.layers.conv2d(input,out_size,3,
-			padding = "SAME", activation = tf.nn.relu,
+with tf.device('/device:GPU:0'):
+	def buildConv2DLayer(input, out_size):
+		conv = tf.layers.conv2d(input,out_size,3,
+				padding = "SAME", activation = tf.nn.relu,
+				kernel_initializer = tf.contrib.layers.xavier_initializer())
+
+		return tf.layers.batch_normalization(conv,training = True, fused = True)
+
+	def buildMaxPool(input):
+		return tf.layers.max_pooling2d(input, 3, 2, padding = "SAME")
+
+	img_size = 28
+	channels = 1
+	classes = 100
+	x = tf.placeholder(tf.float32, shape = [None,img_size,img_size,channels])
+	y_label = tf.placeholder(tf.int64, shape = [None])
+	y = tf.one_hot(y_label, depth = classes, dtype = tf.float32)
+
+	x_norm = tf.layers.batch_normalization(x,training = True)
+
+	conv1_1 = buildConv2DLayer(x_norm,64)
+	conv1_2 = buildConv2DLayer(conv1_1,64)
+	maxpool1 = buildMaxPool(conv1_2)
+
+	conv2_1 = buildConv2DLayer(maxpool1,128)
+	conv2_2 = buildConv2DLayer(conv2_1,128)
+	maxpool2 = buildMaxPool(conv2_2)
+
+	conv3_1 = buildConv2DLayer(maxpool2,128)
+	conv3_2 = buildConv2DLayer(conv3_1,128)
+	maxpool3 = buildMaxPool(conv3_2)
+
+	conv4_1 = buildConv2DLayer(maxpool3,256)
+	conv4_2 = buildConv2DLayer(conv4_1,256)
+	maxpool4 = buildMaxPool(conv4_2)
+
+	fc_1 = tf.layers.dense(maxpool4,1024,activation = tf.nn.relu,
+			kernel_initializer = tf.contrib.layers.xavier_initializer())
+	fc_2 = tf.layers.dense(fc_1,100,
 			kernel_initializer = tf.contrib.layers.xavier_initializer())
 
-	return tf.layers.batch_normalization(conv,training = True)
+	y_pred = tf.nn.softmax(fc_2)
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=fc_2, labels=y))
 
-def buildMaxPool(input):
-	return tf.layers.max_pooling2d(input, 3, 2, padding = "SAME")
+	learning_rate = 0.03
+	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-img_size = 28
-channels = 1
-classes = 100
-x = tf.placeholder(tf.float32, shape = [None,img_size,img_size,channels])
-y_label = tf.placeholder(tf.int64, shape = [None])
-y = tf.one_hot(y_label, depth = classes, dtype = tf.float32)
-
-x_norm = tf.layers.batch_normalization(x,training = True)
-
-conv1_1 = buildConv2DLayer(x_norm,64)
-conv1_2 = buildConv2DLayer(conv1_1,64)
-maxpool1 = buildMaxPool(conv1_2)
-
-conv2_1 = buildConv2DLayer(maxpool1,128)
-conv2_2 = buildConv2DLayer(conv2_1,128)
-maxpool2 = buildMaxPool(conv2_2)
-
-conv3_1 = buildConv2DLayer(maxpool2,128)
-conv3_2 = buildConv2DLayer(conv3_1,128)
-maxpool3 = buildMaxPool(conv3_2)
-
-conv4_1 = buildConv2DLayer(maxpool3,256)
-conv4_2 = buildConv2DLayer(conv4_1,256)
-maxpool4 = buildMaxPool(conv4_2)
-
-fc_1 = tf.layers.dense(maxpool4,1024,activation = tf.nn.relu,
-		kernel_initializer = tf.contrib.layers.xavier_initializer())
-fc_2 = tf.layers.dense(fc_1,100,
-		kernel_initializer = tf.contrib.layers.xavier_initializer())
-
-y_pred = tf.nn.softmax(fc_2)
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=fc_2, labels=y))
-
-learning_rate = 0.03
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-correct_prediction = tf.equal(tf.argmax(y_pred, 1), y_label)
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	correct_prediction = tf.equal(tf.argmax(y_pred, 1), y_label)
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
